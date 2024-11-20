bb5d32429a9f3a6542950cf8b9228674cc4ab599
Matheus Bruhns Bastos <matbbastos@gmail.com>
2024-11-20 19:39:48 -0300

    ref: add first unfinished version of refactored parquet generation

--- src/nc_to_parquet.py
+++ src/nc_to_parquet.py
@@ -1,41 +1,18 @@
-import netCDF4 as nc
+import json
+import logging
+import time
+from argparse import ArgumentParser
+from collections.abc import Sequence
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
-from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
 
-# Função para extrair os dados de precipitação
-def extract_precipitation(file_path, target_lat, target_lon):
-    try:
-        dataset = nc.Dataset(file_path, 'r')
-    except OSError as e:
-        print(f"Erro ao abrir o arquivo: {e}")
-        return None
+from nc_to_csv_timeseries import CLIMATE_MODELS, extract_precipitation
 
-    latitudes = dataset.variables['lat'][:]
-    longitudes = dataset.variables['lon'][:]
-    times = dataset.variables['time'][:]
-    precipitation = dataset.variables['pr'][:]
 
-    lat_index = np.abs(latitudes - target_lat).argmin()
-    lon_index = np.abs(longitudes - target_lon).argmin()
-
-    time_units = dataset.variables['time'].units
-    reference_date_str = time_units.split('since')[1].strip()
-    reference_date = datetime.strptime(reference_date_str, "%Y-%m-%dT%H:%M:%S")
-    dates = [reference_date + timedelta(days=float(t)) for t in times]
-
-    precipitation_series = [
-        (dates[t_index], float(precipitation[t_index, lat_index, lon_index]))
-        for t_index in range(len(dates))
-        if not np.ma.is_masked(precipitation[t_index, lat_index, lon_index])
-    ]
-
-    dataset.close()
-    return precipitation_series
-
-# Função para calcular os índices climáticos
-def calculate_indices(df):
+def calculate_indices(df: pd.DataFrame):
+    # Reference: http://etccdi.pacificclimate.org/list_27_indices.shtml
     df['year'] = df['date'].dt.year
     df['month'] = df['date'].dt.month
     df_wet_days = df[df['precipitation'] >= 1]
@@ -79,55 +56,84 @@ def calculate_indices(df):
 
     return result_df
 
-# Carregar as coordenadas corrigidas do arquivo CSV
-coords_df = pd.read_csv('coordenadas_capitais_validas.csv')
 
-# Lista dos modelos
-models = [
-    'ACCESS-CM2', 'ACCESS-ESM1-5', 'CMCC-ESM2', 'EC-EARTH3', 'GFDL-CM4', 'GFDL-ESM4',
-    'HadGEM3-GC31-LL', 'INM-CM4_8', 'INM-CM5', 'IPSL-CM6A-LR', 'KACE', 'KIOST', 'MIROC6',
-    'MPI-ESM1-2', 'MRI-ESM2', 'NESM3', 'NorESM2-MM', 'TaiESM1', 'UKESM1-0-LL'
-]
+def main(args):
+    setup_start: float = time.perf_counter()
+    logging.basicConfig(
+        format="%(asctime)s    %(levelname)-8.8s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.DEBUG,
+    )
+    if args.quiet == 1:
+        logging.getLogger().setLevel(logging.INFO)
+    elif args.quiet == 2:
+        logging.getLogger().setLevel(logging.WARNING)
+    elif args.quiet >= 3:
+        logging.getLogger().setLevel(logging.ERROR)
 
-# DataFrame para armazenar todos os dados
-all_data = []
+    coordinates_path: Path = Path(args.coordinates)
+    if not coordinates_path.is_file():
+        logging.critical(
+            f"File with cities coordinates not found at '{coordinates_path.resolve()}'")
+        return None
+    with open(coordinates_path) as file:
+        city_coordinates: dict[str, dict[str, Sequence[float]]] = json.load(file)
+    logging.info(f"Setup time: {round(1000*(time.perf_counter() - setup_start))}ms")
 
-counter = 0
+    all_data = []
+    counter = 0
+    for city, details in city_coordinates.items():
+        latitude = details["Nearest Coordinates"][0]
+        longitude = details["Nearest Coordinates"][1]
 
-# Loop sobre as cidades e modelos
-for i, row in coords_df.iterrows():
-    city = row['Capital']
-    target_lat = row['Nearest Latitude']
-    target_lon = row['Nearest Longitude']
-    print(counter)
+        for model in CLIMATE_MODELS:
+            counter += 1
+            files = {
+                'Histórico': f'{model}-pr-hist.nc',
+                'SSP245': f'{model}-pr-ssp245.nc',
+                'SSP585': f'{model}-pr-ssp585.nc'
+            }
 
-    for model in models:
-        counter += 1
-        files = {
-            'Histórico': f'{model}-pr-hist.nc',
-            'SSP245': f'{model}-pr-ssp245.nc',  # Cenário adicionado
-            'SSP585': f'{model}-pr-ssp585.nc'
-        }
+            for scenario, file_path in files.items():
+                series = extract_precipitation(Path(file_path), latitude, longitude)
+                if series:
+                    dates, values = zip(*series)
+                    df = pd.DataFrame({
+                        'date': pd.to_datetime(dates),
+                        'precipitation': values})
+                    indices = calculate_indices(df)
 
-        for scenario, file_path in files.items():
-            series = extract_precipitation(file_path, target_lat, target_lon)
-            if series:
-                dates, values = zip(*series)
-                df = pd.DataFrame({'date': pd.to_datetime(dates), 'precipitation': values})
-                indices = calculate_indices(df)
+                    # Adiciona os dados da cidade, modelo, cenário, latitude e longitude
+                    indices['City'] = city
+                    indices['Model'] = model
+                    indices['Scenario'] = scenario
+                    indices['Latitude'] = latitude
+                    indices['Longitude'] = longitude
 
-                # Adiciona os dados da cidade, modelo, cenário, latitude e longitude
-                indices['City'] = city
-                indices['Model'] = model
-                indices['Scenario'] = scenario
-                indices['Latitude'] = target_lat
-                indices['Longitude'] = target_lon
+                    # Append the data to the main dataframe
+                    all_data.append(indices)
 
-                # Append the data to the main dataframe
-                all_data.append(indices)
+    # Concatenar todos os dados em um único DataFrame
+    all_data_df = pd.concat(all_data, ignore_index=True)
 
-# Concatenar todos os dados em um único DataFrame
-all_data_df = pd.concat(all_data, ignore_index=True)
+    # Salvar o DataFrame como um arquivo Parquet com nome atualizado
+    all_data_df.to_parquet('climate_indices_with_corrected_coordinates_V3.parquet', index=False)
 
-# Salvar o DataFrame como um arquivo Parquet com nome atualizado
-all_data_df.to_parquet('climate_indices_with_corrected_coordinates_V3.parquet', index=False)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    # parser.add_argument(
+    #     "input", type=str, metavar="path/to/input",
+    #     help="path to a directory where the input NetCDF4 files are stored")
+    # parser.add_argument(
+    #     "-o", "--output", type=str, metavar="path/to/output", default="output",
+    #     help="path to a directory where the output files will be saved. "
+    #     "Defaults to 'output'")
+    parser.add_argument(
+        "-q", "--quiet", action="count", default=0,
+        help="turn on quiet mode (cumulative), which hides log entries of levels lower "
+        "than INFO/WARNING")
+    parser.add_argument(
+        "-c", "--coordinates", type=str, metavar="path/to/coordinates.json",
+        help="path to a JSON file containing coordinates of the desired cities")
+    main(parser.parse_args())
