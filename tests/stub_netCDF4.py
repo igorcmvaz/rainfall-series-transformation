170066c34b261a8cf4a2405d6f94a14877387d2d
Matheus Bruhns Bastos <matbbastos@gmail.com>
2025-02-12 20:30:14 -0300

    test: use same reference distribution for samples

--- tests/stub_netCDF4.py
+++ tests/stub_netCDF4.py
@@ -11,6 +11,7 @@ SAMPLE_NC_PATH = Path(__file__).parent / "sample.nc"
 class NetCDFStubGenerator:
 
     rng = np.random.default_rng(seed=42)
+    precipitation_sample = rng.uniform(size=(100, 168, 162))
 
     @classmethod
     def create_sample_stub(cls, output_path: Path) -> None:
@@ -43,8 +44,8 @@ class NetCDFStubGenerator:
 
             longitudes[:] = [-74.125 + 0.25*step for step in range(162)]
             latitudes[:] = [-34.125 + 0.25*step for step in range(168)]
-            times[:] = np.arange(0, 100, 1)
-            precipitation[:, :, :] = cls.rng.uniform(size=(100, 168, 162))
+            times[:] = np.arange(100)
+            precipitation[:, :, :] = cls.precipitation_sample
 
     @classmethod
     def create_sample_variables(cls) -> dict[str, ParsedVariable]:
@@ -56,9 +57,9 @@ class NetCDFStubGenerator:
                 "degrees_north", np.ma.masked_array(
                     [-34.125 + 0.25*step for step in range(168)])),
             "time": ParsedVariable(
-                "days since 2020-01-01T00:00:00", np.ma.masked_array(np.arange(0, 100, 1))),
+                "days since 2020-01-01T00:00:00", np.ma.masked_array(np.arange(100))),
             "pr": ParsedVariable(
-                "mm", np.ma.masked_array(cls.rng.uniform(size=(100, 168, 162)))),
+                "mm", np.ma.masked_array(cls.precipitation_sample)),
         }
 
 
