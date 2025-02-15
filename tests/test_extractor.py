c628144bafb2861f40d4dfe91fe8a82f5c501c9f
Matheus Bruhns Bastos <matbbastos@gmail.com>
2025-02-15 18:11:51 -0300

    test: improve extractor tests with subtests where relevant

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -7,6 +7,9 @@ from errors import InvalidTargetCoordinatesError
 from extractor import NetCDFExtractor
 from tests.stub_netCDF4 import SAMPLE_NC_PATH, NetCDFStubGenerator
 
+LATITUDES = [-34.125 + 0.25*step for step in range(16)]
+LONGITUDES = [-74.125 + 0.25*step for step in range(12)]
+
 
 class TestNetCDFExtractor(unittest.TestCase):
 
@@ -42,26 +45,36 @@ class TestNetCDFExtractor(unittest.TestCase):
         self.extractor.variables["time"].units = ORIGINAL_UNITS
 
     def test_find_coordinates_indices_present(self):
-        self.assertTupleEqual(
-            self.extractor._find_coordinates_indices(-34.125, -74.125), (0, 0))
+        for latitude_index, target_latitude in enumerate(LATITUDES):
+            for longitude_index, target_longitude in enumerate(LONGITUDES):
+                with self.subTest(coordinates=(target_latitude, target_longitude)):
+                    self.assertTupleEqual(
+                        self.extractor._find_coordinates_indices(
+                            target_latitude, target_longitude),
+                        (latitude_index, longitude_index))
 
     def test_find_coordinates_indices_absent(self):
         with self.assertRaises(InvalidTargetCoordinatesError):
             self.extractor._find_coordinates_indices(-34.005, -74.977)
 
     def test_relative_to_absolute_date(self):
-        LATITUDE_INDEX = 0
-        LONGITUDE_INDEX = 0
         expected_values = NetCDFStubGenerator.create_sample_variables()
-        expected_output = np.array([(
-            datetime(2020, 1, 1) + timedelta(days=float(t)),
-            expected_values["pr"].values[t, LATITUDE_INDEX, LONGITUDE_INDEX]
-            ) for t in range(100)])
 
-        result = self.extractor._relative_to_absolute_date(
-            self.extractor.variables["pr"].values[:, LATITUDE_INDEX, LONGITUDE_INDEX])
+        for latitude_index in range(16):
+            for longitude_index in range(12):
+                with self.subTest(indices=(latitude_index, longitude_index)):
 
-        self.assertListEqual(expected_output.tolist(), result.tolist())
+                    expected_output = np.array([
+                        (
+                            datetime(2020, 1, 1) + timedelta(days=float(t)),
+                            expected_values["pr"].values[t, latitude_index, longitude_index]
+                        ) for t in range(100)])
+
+                    result = self.extractor._relative_to_absolute_date(
+                        self.extractor.variables[
+                            "pr"].values[:, latitude_index, longitude_index])
+
+                    self.assertListEqual(expected_output.tolist(), result.tolist())
 
 
 if __name__ == '__main__':
